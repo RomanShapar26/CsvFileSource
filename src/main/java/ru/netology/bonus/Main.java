a0a55ee707b84c0f227d2982a1908f322d61f5f6
Роман Шапарь <romanshaparution@gmail.com>
2022-03-13 08:39:19 +0300

    modif

---
+++ src/main/java/ru/netology/bonus/Main.java
@@ -0,0 +1,18 @@
+package ru.netology.bonus;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        boolean passed = expected == actual;
+
+        System.out.println(passed);
+
+    }
+}
